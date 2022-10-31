6b3bb678969fe2f9b66fb22599faf11edcf4ebc8
Alina Malyutina <linamalutina1@gmail.com>
2022-10-31 15:24:09 +0300

    completed third task - check number is even

---
+++ task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("This program defines is your number even or not.");
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine($"Number {number} is even");
+
+if (number % 2 == 1) Console.WriteLine($"Number {number} is odd");
+
+if (number % 2 == -1) Console.WriteLine($"Number {number} is odd");
