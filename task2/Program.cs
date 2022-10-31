eca853b50ef4e8fd93fffffbdf3c9b0c506a094a
Alina Malyutina <linamalutina1@gmail.com>
2022-10-31 15:00:29 +0300

    complete first task

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("This program defines max and min of two numbers.");
+Console.Write("Enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+int min = num1;
+
+if(num2 > max)
+{
+    max = num2;
+}
+else
+{
+    min = num2;
+}
+Console.WriteLine($"Max number is {max}, min is {min}.");
